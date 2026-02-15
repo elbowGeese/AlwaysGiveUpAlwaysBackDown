f64b6833d8462394e6503d0e8371e2574fbd52b2
elbowGeese <eapricemail@gmail.com>
2026-02-14 16:55:26 -0800

    size stuff

--- AlwaysGiveUpAlwaysBackDown/Assets/IndividualWorkspaces/Eb/SizeHandler.cs
+++ AlwaysGiveUpAlwaysBackDown/Assets/IndividualWorkspaces/Eb/SizeHandler.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
+using System;
 
 public class SizeHandler : MonoBehaviour
 {
     public DustReceiver dustReceiver;
+    public BunnyController bc;
+
+    public AnimationCurve jumpForceCurve;
+    public AnimationCurve scaleCurve;
+    public AnimationCurve smallPRadius, mediumPRadius, largePRadius;
+
+    public ParticleSystem smallP1, smallP2, mediumP, largeP1, largeP2;
 
     void Start()
     {
-        
+        dustReceiver.onDustReceived += UpdateSize;
+        UpdateSize();
     }
 
-    public void UpdateSize(float size)
+    void OnDisable()
     {
+        dustReceiver.onDustReceived -= UpdateSize;
+    }
 
+    public void UpdateSize()
+    {
+        float dustAmount = dustReceiver.DustAmount;
+
+        // scale
+        float currentScale = scaleCurve.Evaluate(dustAmount);
+        transform.localScale = Vector2.one * currentScale;
+
+        // grounded ray
+        bc.groundedRayDistance = currentScale;
+
+        // jump force
+        bc.jumpForce = jumpForceCurve.Evaluate(dustAmount);
+
+        // particles
+        float sRadius = smallPRadius.Evaluate(dustAmount);
+        var smallP1ShapeModule = smallP1.shape;
+        smallP1ShapeModule.radius = sRadius;
+        var smallP2ShapeModule = smallP2.shape;
+        smallP2ShapeModule.radius = sRadius;
+
+        float mRadius = mediumPRadius.Evaluate(dustAmount);
+        var mediumPShapeModule = mediumP.shape;
+        mediumPShapeModule.radius = mRadius;
+
+        float lRadius = largePRadius.Evaluate(dustAmount);
+        var largeP1ShapeModule = largeP1.shape;
+        largeP1ShapeModule.radius = lRadius;
+        var largeP2ShapeModule = largeP2.shape;
+        largeP2ShapeModule.radius = lRadius;
     }
 }
