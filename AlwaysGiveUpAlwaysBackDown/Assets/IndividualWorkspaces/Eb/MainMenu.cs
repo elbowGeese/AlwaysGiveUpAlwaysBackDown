655be1f5505f46e216d82cc0436b751b6faf0c33
elbowGeese <eapricemail@gmail.com>
2026-02-15 09:34:24 -0800

    main menu greybox

--- AlwaysGiveUpAlwaysBackDown/Assets/IndividualWorkspaces/Eb/MainMenu.cs
+++ AlwaysGiveUpAlwaysBackDown/Assets/IndividualWorkspaces/Eb/MainMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,25 +11,64 @@ public class MainMenu : MonoBehaviour
     void Start()
     {
         mode = MenuMode.NONE;
+        CloseAllPanels();
     }
 
     public void Play()
     {
-
+        // change scene
+        SceneManager.LoadScene(1);
     }
 
     public void HowTo()
     {
-
+        if(mode != MenuMode.HOWTO)
+        {
+            CloseAllPanels();
+            howToPanel.SetActive(true);
+            mode = MenuMode.HOWTO;
+        }
+        else
+        {
+            howToPanel.SetActive(false);
+            mode = MenuMode.NONE;
+        }
     }
 
     public void Settings()
     {
-
+        if (mode != MenuMode.SETTINGS)
+        {
+            CloseAllPanels();
+            settingsPanel.SetActive(true);
+            mode = MenuMode.SETTINGS;
+        }
+        else
+        {
+            settingsPanel.SetActive(false);
+            mode = MenuMode.NONE;
+        }
     }
 
     public void Credits()
     {
+        if (mode != MenuMode.CREDITS)
+        {
+            CloseAllPanels();
+            creditsPanel.SetActive(true);
+            mode = MenuMode.CREDITS;
+        }
+        else
+        {
+            settingsPanel.SetActive(false);
+            mode = MenuMode.NONE;
+        }
+    }
 
+    private void CloseAllPanels()
+    {
+        howToPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
     }
 }
