437f6b4b84a5f6c69613efe1846e260c4a1eabea
katalover420 <ownedbyanowl@gmail.com>
2026-02-14 18:13:32 -0800

    updated fan prefab

--- AlwaysGiveUpAlwaysBackDown/Assets/IndividualWorkspaces/Panhya/FanScript.cs
+++ AlwaysGiveUpAlwaysBackDown/Assets/IndividualWorkspaces/Panhya/FanScript.cs
@@ -5,12 +5,17 @@ public class FanScript : MonoBehaviour
     public GameObject player;
     Rigidbody2D playerRB;
     public float fanStrength;
+    public int DustRequiredToDropThrough;
+    
+    public DustReceiver dustReceiverScript;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindAnyObjectByType<BunnyController>().gameObject;
         playerRB = player.GetComponent<Rigidbody2D>();
+        dustReceiverScript = player.GetComponent<DustReceiver>();
     }
 
     // Update is called once per frame
@@ -25,9 +30,17 @@ public class FanScript : MonoBehaviour
         Debug.Log("COLLISION HAPPENED");
         if (collision.transform.tag == "Player")
         {
-            Debug.Log("fan moving bunny up");
-            
-            playerRB.AddForceY(fanStrength);
+            if (dustReceiverScript.DustAmount >= DustRequiredToDropThrough)
+            {
+                
+            }
+        
+            else
+            {
+                playerRB.AddForceY(fanStrength);
+
+            }
+
         }
     }
    
