4cc6648083d0c83dd0e29c4247f48b56db0dd9fa
katalover420 <ownedbyanowl@gmail.com>
2026-02-14 16:56:03 -0800

    fan script made

---
+++ AlwaysGiveUpAlwaysBackDown/Assets/IndividualWorkspaces/Panhya/FanScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FanScript : MonoBehaviour
+{
+    public GameObject player;
+    Rigidbody2D playerRB;
+    public float fanStrength;
+    public float editBunnyMass;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindAnyObjectByType<BunnyController>().gameObject;
+        playerRB = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("COLLISION HAPPENED");
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("fan moving bunny up");
+            playerRB.mass += editBunnyMass;
+            playerRB.AddForceY(fanStrength);
+        }
+    }
+   
+}
